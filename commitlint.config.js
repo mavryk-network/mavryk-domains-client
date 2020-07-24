45557594ebcfe8083eae8695476542026a231f9a
Necroskillz <necroskillz@gmail.com>
2020-07-24 21:18:36 +0200

    build: generate changelog

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
+    rules: {
+        'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'release']],
+    },
 };
