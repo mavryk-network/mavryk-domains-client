5072432bafe31864fc1a3744876120f430da08d7
Necroskillz <necroskillz@gmail.com>
2020-08-03 20:23:42 +0200

    test(core): make tests independent on timezone

--- packages/core/test/rpc-data/encoders/date-encoder.spec.ts
+++ packages/core/test/rpc-data/encoders/date-encoder.spec.ts
@@ -9,7 +9,9 @@ describe('DateEncoder', () => {
 
     describe('encode()', () => {
         it('should encode date', () => {
-            expect(encoder.encode(new Date(2020, 9, 1, 22, 5, 0))).toBe('2020-10-01T20:05:00.000Z');
+            expect(encoder.encode(new Date(new Date(2020, 9, 1, 22, 5, 0).getTime() - new Date().getTimezoneOffset() * 60000))).toBe(
+                '2020-10-01T22:05:00.000Z'
+            );
         });
 
         it('should return null for null', () => {
@@ -19,7 +21,9 @@ describe('DateEncoder', () => {
 
     describe('decode()', () => {
         it('should decode date', () => {
-            expect(encoder.decode('2020-10-01T20:05:00.000Z')?.toString()).toBe(new Date(2020, 9, 1, 22, 5, 0).toString());
+            expect(encoder.decode('2020-10-01T20:05:00.000Z')?.toString()).toBe(
+                new Date(new Date(2020, 9, 1, 20, 5, 0).getTime() - new Date().getTimezoneOffset() * 60000).toString()
+            );
         });
 
         it('should return null for null', () => {
--- packages/core/test/tracing/tracer.spec.ts
+++ packages/core/test/tracing/tracer.spec.ts
@@ -10,7 +10,7 @@ describe('Tracer', () => {
         beforeEach(() => {
             consoleSpy = jest.spyOn(global.console, 'debug').mockImplementation(void 0);
 
-            MockDate.set(new Date(2020, 9, 11, 20, 10, 35));
+            MockDate.set(new Date(new Date(2020, 9, 11, 20, 10, 35).getTime() - new Date().getTimezoneOffset() * 60000));
 
             tracer = new ConsoleTracer();
         });
@@ -22,7 +22,7 @@ describe('Tracer', () => {
         it('should log to console', () => {
             tracer.trace('some msg', 'arg1', 'arg2');
 
-            expect(consoleSpy).toHaveBeenCalledWith('[2020-10-11T18:10:35.000Z] Trace', 'some msg', 'arg1', 'arg2');
+            expect(consoleSpy).toHaveBeenCalledWith('[2020-10-11T20:10:35.000Z] TezosDomains', 'some msg', 'arg1', 'arg2');
         });
     });
 });
