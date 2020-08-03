4a4edf1483624d5e3446feaebc0425ca9741e742
Necroskillz <necroskillz@gmail.com>
2020-08-03 22:41:33 +0200

    chore: add indent eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    ignorePatterns: ['**/coverage', '**/dist', '**/node_modules', '**/*.config.ts',  '**/*.config.js', '.eslintrc.js'],
+    ignorePatterns: ['**/coverage', '**/dist', '**/node_modules', '**/*.config.ts', '**/*.config.js', '.eslintrc.js'],
     parserOptions: {
         tsconfigRootDir: __dirname,
         project: ['./packages/**/tsconfig.eslint.json', './integration-tests/tsconfig.eslint.json'],
-        sourceType: "module"
+        sourceType: 'module',
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
     rules: {
         '@typescript-eslint/no-non-null-assertion': 0,
         '@typescript-eslint/no-explicit-any': 0,
@@ -17,8 +17,12 @@ module.exports = {
         '@typescript-eslint/no-unsafe-call': 0,
         '@typescript-eslint/no-unsafe-member-access': 0,
         '@typescript-eslint/restrict-template-expressions': 0,
-        '@typescript-eslint/explicit-module-boundary-types': [2, {
-            allowArgumentsExplicitlyTypedAsAny: true
-        }]
-    }
+        '@typescript-eslint/explicit-module-boundary-types': [
+            2,
+            {
+                allowArgumentsExplicitlyTypedAsAny: true,
+            },
+        ],
+        indent: ['error', 4, { SwitchCase: 1 }],
+    },
 };
