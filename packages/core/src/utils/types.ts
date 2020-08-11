c68de3302a3ecd4dac4788aadd1d71ca2f8f80af
Necroskillz <necroskillz@gmail.com>
2020-08-11 16:26:18 +0200

    chore: remove unused types

--- packages/core/src/utils/types.ts
+++ packages/core/src/utils/types.ts
@@ -1,4 +1,2 @@
 export type Constructable<T> = new (...args: unknown[]) => T;
 export type Exact<T> = { [K in keyof T]: T[K] };
-export type ReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
-export type Unpromise<T> = T extends Promise<infer U> ? U : T;
