ae3ba0164e9e469df1174397a947077844256f7a
Necroskillz <necroskillz@gmail.com>
2020-10-06 15:53:05 +0200

    build: exclude scripts from typedoc

--- .typedoc.js
+++ .typedoc.js
@@ -2,7 +2,7 @@ module.exports = {
     mode: 'modules',
     out: 'dist/typedoc',
     readme: 'README.md',
-    exclude: ['**/dist/**', '**/node_modules/**', '**/rollup*.ts', '**/test/**', 'integration-tests/**/*.ts'],
+    exclude: ['**/dist/**', '**/node_modules/**', '**/rollup*.ts', '**/test/**', 'integration-tests/**/*.ts', 'scripts/**'],
     lernaExclude: [],
     name: 'Tezos Domains',
     excludePrivate: true,
