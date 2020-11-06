7083806f7dd91222442b63150c31532f5946abbd
Necroskillz <necroskillz@gmail.com>
2020-11-06 14:09:18 +0100

    chore: take supported networks from BuiltInAddresses

--- packages/core/src/utils/support.ts
+++ packages/core/src/utils/support.ts
@@ -1,6 +1,9 @@
+import { BuiltInAddresses } from '../address-book/built-in-addresses';
+
 export type SupportedNetworkType = 'carthagenet' | 'delphinet' | 'custom';
 
-const SUPPORTED_NETWORKS = ['carthagenet', 'delphinet', 'custom'];
+// TODO: dont filter mainnet when the contracts are deployed
+const SUPPORTED_NETWORKS = Object.keys(BuiltInAddresses).filter(n => n !== 'mainnet');
 
 /**
  * Can be used for feature toggles.
