6aeba24ee4d2ab22c7bd913492d67a5d75c70dff
Alex <alexseleznov@madfish.solutions>
2022-10-09 12:43:40 +0000

    fix(core): import 'crypto' replaced with conditional require

--- packages/core/src/utils/crypto.ts
+++ packages/core/src/utils/crypto.ts
@@ -1,7 +1,9 @@
-import { randomInt } from 'crypto';
+import type { randomInt as randomIntType } from 'crypto';
 
 export function generateNonce(): number {
     if (typeof process != 'undefined' && typeof process.pid == 'number' && process.versions && process.versions.node) {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const randomInt : typeof randomIntType = require('crypto').randomInt;
         return randomInt(0xFFFFFFFFFFFF);
     } else {
         return window.crypto.getRandomValues(new Uint32Array(1))[0];
