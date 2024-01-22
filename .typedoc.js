d6e9367f92b18a1f741f82ca1f7e65a769daf8b1
Tristan Allaire <tristan.allaire@alarch.fr>
2024-01-22 10:46:46 +0100

    fix: gitlabci pipeline fix attempt

--- .typedoc.js
+++ .typedoc.js
@@ -1,10 +1,7 @@
 module.exports = {
-    mode: 'modules',
     out: 'dist/typedoc',
     readme: 'README.md',
     exclude: ['**/dist/**', '**/node_modules/**', '**/rollup*.ts', '**/test/**', 'integration-tests/**/*.ts', 'scripts/**'],
-    lernaExclude: [],
     name: 'Mavryk Domains',
     excludePrivate: true,
-    excludeNotExported: true,
 };
